5f09a17db5e4dba3e2686675cd150ac2165a7a63
Lucas Layman <llayman@fc-md.umd.edu>
2016-09-12 16:39:57 -0400

    first commit

---
+++ src/org/cese/madcap/analysis/CountingServlet.java
@@ -0,0 +1,12 @@
+package org.cese.madcap.analysis;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CountingServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
