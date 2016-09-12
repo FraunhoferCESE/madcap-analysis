4264fe6cbc1febd957ea75dc9016547f0615479d
Lucas Layman <llayman@fc-md.umd.edu>
2016-09-12 17:43:54 -0400

    Added an Objectify example. Added Maven dependency management.

--- src/org/cese/madcap/analysis/CountingServlet.java
+++ src/org/cese/madcap/analysis/CountingServlet.java
@@ -1,12 +1,26 @@
 package org.cese.madcap.analysis;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.cese.madcap.analysis.OfyService.ofy;
 import javax.servlet.http.*;
+
+import org.cese.madcap.analysis.models.ProbeEntry;
 
 @SuppressWarnings("serial")
 public class CountingServlet extends HttpServlet {
+	private static final Logger logger = Logger.getLogger(CountingServlet.class.getName());
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		try {
+		int count = ofy().load().type(ProbeEntry.class).count();
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("# of ProbeEntry rows: " + count);
+		}
+		catch (IOException e) {
+			 logger.log(Level.SEVERE, e.toString(), e);
+		}
 	}
 }
---
+++ src/org/cese/madcap/analysis/models/ProbeDataSet.java
@@ -0,0 +1,54 @@
+package org.cese.madcap.analysis.models;
+
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class ProbeDataSet implements Serializable {
+
+    //attributes
+    @Id
+    private Long id;
+    private Collection<ProbeEntry> entries;
+    private Long timestamp;
+    private String userId;
+
+
+    //getters and setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setEntryList(List<ProbeEntry> entries) {
+        this.entries = entries;
+    }
+
+    public Collection<ProbeEntry> getEntryList() {
+        return entries;
+    }
+}
