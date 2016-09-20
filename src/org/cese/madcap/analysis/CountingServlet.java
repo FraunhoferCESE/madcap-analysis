eaa8221e012af01dd64e5e3411d09a4257742403
Stefan Hintzen <SHintzen@FCMD.local>
2016-09-20 16:20:05 -0400

    still good code. Crashes as soon as Google annotations get added

--- src/org/cese/madcap/analysis/CountingServlet.java
+++ src/org/cese/madcap/analysis/CountingServlet.java
@@ -1,25 +1,66 @@
 package org.cese.madcap.analysis;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import static org.cese.madcap.analysis.OfyService.ofy;
+
 import javax.servlet.http.*;
 
 import org.cese.madcap.analysis.models.ProbeEntry;
 
-@SuppressWarnings("serial")
-public class CountingServlet extends HttpServlet {
-	private static final Logger logger = Logger.getLogger(CountingServlet.class.getName());
+import com.googlecode.objectify.ObjectifyService;
 
+
+@SuppressWarnings("serial")
+
+public class CountingServlet extends HttpServlet{
+	
+	private boolean fill_db_manually = false;
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
 		try {
-			int count = ofy().load().type(ProbeEntry.class).count();
+			ObjectifyService.register(ProbeEntry.class);
+			int count = ofy().load().type(ProbeEntry.class).limit(49).count();
 			resp.setContentType("text/plain");
+			if (fill_db_manually) {
+				fillLocalDatabase(50);
+			}
 			resp.getWriter().println("# of ProbeEntry rows: " + count);
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, e.toString(), e);
+			resp = myProbeEntries(49, resp);
+		} catch (IOException e) {}
+	}
+	
+	
+	public void fillLocalDatabase(int ammount) {
+		ProbeEntry[] p = new ProbeEntry[ammount];
+		for (int i = 0; i < ammount; i++) {
+			p[i] = new ProbeEntry();
+			p[i].setId("" + i);
+			p[i].setProbeType("" + i);
+			p[i].setSensorData("" + i);
+			p[i].setTimestamp((long) i);
+			p[i].setUserID("" + i);
 		}
+		ofy().save().entities(p).now();
+	}
+	
+	
+	public HttpServletResponse myProbeEntries(final int ammount, final HttpServletResponse resp)	{
+	ObjectifyService.begin();
+	List<ProbeEntry> probeList = ofy().load().type(ProbeEntry.class).limit(ammount).list();
+	resp.setContentType("text/plain");
+	for(int i=0; i<probeList.size(); i++){
+		ProbeEntry probe = probeList.get(i);
+		String csv = probe.getTimestamp()+","+probe.getProbeType()+","+probe.getSensorData()+","+probe.getUserID()+",";
+		try {
+			resp.getWriter().write(csv);
+			//resp.addHeader(probe.getId(), csv);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	return resp;
 	}
 }
