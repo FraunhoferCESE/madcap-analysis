006758af165dc11f28c9badbf8ca0f236332080b
Stefan Hintzen <SHintzen@FCMD.local>
2016-10-13 16:12:57 -0400

    added loading animations and messages

---
+++ src/main/webapp/js/madcap_control.component.js
@@ -0,0 +1,8 @@
+angular.
+module('madcapControl').
+  component('madcapControl', {
+    templateUrl: 'html/madcap_control_view.template.html',
+    controller: function madcapController() {
+    	"use strict"; 	
+    } 
+});
